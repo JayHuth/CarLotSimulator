65d60b43e9913062cf04efa29e68f7ce99b9a68c
Justin Huth <Jayhuth96@gmail.com>
2023-02-14 11:41:01 -0600

    completed the excercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public bool IsDrivable { get; set; }
+
+        Car() 
+        {
+             
+        }
+
+
+
+        //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,17 +6,37 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
-            //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+            //TODO
             //The methods should take one string parameter: the respective noise property
 
 
             //Now that the Car class is created we can instanciate 3 new cars
+
             //Set the properties for each of the cars
+            Car car1 = new Car();
+            car1.Make = "Ford";
+            car1.Model = "Ranger";
+            car1.Year = 2019;
+            car1.IsDrivable = true;
+            car1.EngineNoise = "VROOOOOM";
+            car1.HonkNoise = "BEEP BEEP";
+
+            Car car2 = new Car()
+            {
+                Make = "Toyota",
+                Model = "Tacoma",
+                Year = 2019,
+                IsDrivable = true,
+                EngineNoise = "VROOM vrooom",
+                HonkNoise = "BEEP BEEP beep"
+            };
+
+            Car car3 = new Car(2023, "Ford", "F-150");
+
+
             //Call each of the methods for each car
+
 
             //*************BONUS*************//
 
@@ -28,6 +48,12 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            foreach (var item in myCarLot.ListOfCars)
+            {
+                Console.WriteLine($"Year: {car.Year} / Make: {car.Make} / Model: {car.Model})
+
+            }
         }
     }
 }
